e23edf3f22874a0af01de16a91e1cff983f259de
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2021-06-19 15:38:29 +0800

    Add Stopwatch Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'countdown-timer';
+
+  /**
+   * Track the minutes and seconds fields
+   */
+  @ViewChild('minuteField')
+  minutesField: ElementRef | undefined;
+
+  @ViewChild('secondField')
+  secondsField:  ElementRef | undefined;
+
+  /**
+   * track the values
+   */
+  minutes: number;
+  seconds: number;
+
+  constructor(private decimalPipe: DecimalPipe) {
+    this.minutes = 0;
+    this.seconds = 0;
+  }
+
+  /**
+   * Placeholders for the input field - formatted to have the same number of digits as the
+   * stopwatch display, with the m/s suffixes.
+   */
+  get minutesPlaceholder() {
+    return this.decimalPipe.transform(this.minutes, '2.0') + 'm';
+  }
+
+  get secondsPlaceholder() {
+    return this.decimalPipe.transform(this.seconds, '2.0') + 's';
+  }
+
 }
