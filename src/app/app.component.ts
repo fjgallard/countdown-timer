30fbf8a0adaa3d1c109dc3cc411d009098a962da
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2021-06-27 18:27:14 +0800

    Set a Limit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,6 +63,41 @@ export class AppComponent {
   }
 
   /**
+   * Check if the key being inputted is a valid key and if the input field has a valid value after pressing
+   */
+  validateInput(event: any) {
+    const keyCode = event.which;
+    const isValidInput = this.isValidInput(event.which);
+    if (!isValidInput) {
+      event.stopPropagation();
+      event.preventDefault();
+      return;
+    }
+
+    const numberKeys = this.isNumberKeys(event.which);
+    const fieldValue = event.target.value; 
+
+    if (fieldValue.length > 1 && numberKeys) {
+      event.target.value = "59";
+      event.stopPropagation();
+      event.preventDefault();
+      return;
+    }
+  }
+
+  private isNumberKeys(keyCode: number) {
+    return (keyCode > 47 && keyCode < 58);
+  }
+
+  private isValidInput(keyCode: number) {
+    const arrowKeys = (keyCode >= 37 && keyCode<= 40);
+    const deleteKey = keyCode === 46;
+    const backspaceKey = keyCode === 8;
+
+    return arrowKeys || this.isNumberKeys(keyCode) || deleteKey|| backspaceKey;
+  }
+
+  /**
    * Placeholders for the input field - formatted to have the same number of digits as the
    * stopwatch display, with the m/s suffixes.
    */
